e7020096e8da54293249a98a725e9917c1541088
Daniel Feng <daniel@DanMac.local>
2023-09-05 18:11:09 -0400

    refine params

--- src/detect.py
+++ src/detect.py
@@ -14,12 +14,12 @@ def detect_img(img):
     
     # separate red green yellow color channels and filter out target color saturation & val
     lower_red1 = np.array([0,30,200])
-    upper_red1 = np.array([18,255,255])
+    upper_red1 = np.array([10,255,255])
     lower_red2 = np.array([170,30,200])
     upper_red2 = np.array([180,255,255])
     lower_green = np.array([50,60,200])
     upper_green = np.array([95,255,255])
-    lower_yellow = np.array([18,115,210])
+    lower_yellow = np.array([22,115,210])
     upper_yellow = np.array([33,255,255])
     maskr1 = cv.inRange(hsv, lower_red1, upper_red1)
     maskr2 = cv.inRange(hsv, lower_red2, upper_red2)
@@ -45,15 +45,15 @@ def detect_img(img):
     min_dist = 50
     # hough circle detection with limits on circle size, distance, and perfectness
     red_circles = cv.HoughCircles(maskr, cv.HOUGH_GRADIENT_ALT, dp, min_dist,
-                               param1=30, param2=.3, minRadius=0, maxRadius=50)
+                               param1=30, param2=.5, minRadius=0, maxRadius=1000)
     green_circles = cv.HoughCircles(maskg, cv.HOUGH_GRADIENT_ALT, dp, min_dist,
-                               param1=30, param2=.75, minRadius=2, maxRadius=40)
+                               param1=30, param2=.75, minRadius=0, maxRadius=1000)
     yellow_circles = cv.HoughCircles(masky, cv.HOUGH_GRADIENT_ALT, dp, min_dist,
-                               param1=30, param2=.5, minRadius=0, maxRadius=50)
+                               param1=30, param2=.5, minRadius=0, maxRadius=1000)
     
     # further filtering by calculating variance and mean hue
     circles = [red_circles,yellow_circles,green_circles]
-    masks = [maskr,masky,maskg]
+    
     for i in range(3):
         del_idx = []
         if circles[i] is not None:
@@ -67,12 +67,12 @@ def detect_img(img):
                 bgr_var = np.var(cropped_bgr[:,:,0])+np.var(cropped_bgr[:,:,1])+np.var(cropped_bgr[:,:,2])
                 bgr_var = int(bgr_var)
                 avg_hue,avg_sat,avg_val=rgb_to_hsv(int(np.mean(cropped_bgr[:,:,2])),int(np.mean(cropped_bgr[:,:,1])),int(np.mean(cropped_bgr[:,:,0])))
-                if bgr_var < 0  or (i == 0 and (16 < avg_hue < 160 or avg_sat < 150 or avg_val < 140)) or (i == 1 and (not(18 < avg_hue < 33) or not(200 < avg_val < 255))) or (i == 2 and (not(50 < avg_hue < 95))):
+                if bgr_var < 0  or (i == 0 and (18 < avg_hue < 160 or avg_sat < 120 or avg_val < 170)) or (i == 1 and (not(18 < avg_hue < 33) or not(200 < avg_val < 255))) or (i == 2 and (not(50 < avg_hue < 95))):
                     del_idx.append(j)
-                elif circle[1] > 0.5*height:
+                elif circle[1] > 0.75*height:
                     del_idx.append(j)
                 else:
-                    print(f'[{i}: {circle[0]}]: hue = {avg_hue}, sat = {avg_sat}, val = {avg_val}')
+                    print(f'[{i}: {circle[0]}]: hue = {avg_hue}, sat = {avg_sat}, val = {avg_val}, rad = {rad}')
                 j+=1
         for idx in reversed(del_idx):
             circles[i] = np.delete(circles[i],idx,1)
--- src/visualize.py
+++ src/visualize.py
@@ -5,10 +5,13 @@ import cv2 as cv
 
 # prepare and compute circles
 images = data.images
+sub_image = []
+sub_image.append(images[11])
 for img in images:
     [reds, yellows, greens] = det.detect_img(img)
     circles = [reds,yellows,greens]
     colors = ['RED','YELLOW','GREEN']
+    color_val = [(61,61,255),(0,206,229),(0,174,17)]
 
     # visualize data
     img_cpy = img.copy()
@@ -17,9 +20,11 @@ for img in images:
         if circles[i] is not None:
             j = 0
             for circle in circles[i][0, :]:
-                cv.circle(img_cpy,(int(circle[0]),int(circle[1])),int(circle[2]),(0,255,0),2)
-                cv.putText(img_cpy,colors[i],(int(circle[0]),int(circle[1])),cv.FONT_HERSHEY_SIMPLEX,fontScale=(width/1600),color=(0,0,255),thickness=3)
+                cv.circle(img_cpy,(int(circle[0]),int(circle[1])),int(circle[2]),color_val[i],3)
+                cv.putText(img_cpy,colors[i],(int(circle[0]-circle[2]),int(circle[1]-circle[2])),cv.FONT_HERSHEY_SIMPLEX,fontScale=(width/1600),color=color_val[i],thickness=3)
                 j += 1
                 
     cv.imshow('display',img_cpy)
     cv.waitKey(0)
+
+
