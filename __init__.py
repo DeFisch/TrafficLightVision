b8103b2096c0c62641933f9d8eaa4a448fe3a84a
Daniel Feng <daniel@DanMac.local>
2023-09-05 14:27:54 -0400

    organize files

--- __init__.py
+++
@@ -1,4 +0,0 @@
-# created by Daniel Feng
-# Date: 09/04/2023
-# Traffic Light Detection without Deep Learning
-# 
--- prepare_data.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import cv2 as cv
-
-num_data = 14
-index = np.linspace(1,num_data,num=num_data,dtype=int)
-images = []
-
-for i in index:
-    images.append(cv.imread(f'./data/{i}.jpg'))
